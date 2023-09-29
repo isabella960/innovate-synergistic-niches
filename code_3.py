4e4a567b78b5d9992a5b6a7f24f1fd84bacefef9
Isabella <143600689+isabella960@users.noreply.github.com>
2023-09-29 23:59:19 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,59 @@
+
+    for item in data:
+if __name__ == "__main__":
+
+
+
+    data = generate_random_data()
+    main()
+import random
+def main():
+        print(f"Random Number: {item}")
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+main();
+    });
+}
+    for (let i = 0; i < 10; i++) {
+    return data;
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+function generateRandomData() {
+
+    data.forEach(item => {
+
+    const data = [];
+function main() {    return data;
+    data.forEach(item => {
+function main() {
+
+    const data = [];
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    }
+}
+main();
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+
+    for (let i = 0; i < 10; i++) {
+}def generate_random_data():
+
+def main():
+    return data
+    for item in data:
+import random
+if __name__ == "__main__":
+
+    main()
+
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
